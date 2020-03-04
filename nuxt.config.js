3b6a1bd1564d87cb65bf551cd0397a2a10d601ee
pravin <pravin@veloxsoftech.com>
2020-03-04 14:28:01 +0530

    bootstrap vue plugin added

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,6 +28,7 @@ module.exports = {
   ** Plugins to load before mounting the App
   */
   plugins: [
+  '@/plugins/bootstrap-vue'
   ],
   /*
   ** Nuxt.js dev-modules
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
-
+<div class=""> 
+	<table class="table borderd">
+		<thead>
+			<th>Taes</th>
+		</thead>
+		<tbody>
+			<tr>
+				<td>123213</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
 </template>
 
 <script>
@@ -9,5 +20,24 @@ export default {
   components: {
     Logo
   }
+}
+router: {
+  routes: [
+    {
+      name: 'index',
+      path: '/',
+      component: 'pages/index.vue'
+    },
+    {
+      name: 'user',
+      path: '/user',
+      component: 'pages/user/index.vue'
+    },
+    {
+      name: 'user-one',
+      path: '/user/one',
+      component: 'pages/user/one.vue'
+    }
+  ]
 }
 </script>
